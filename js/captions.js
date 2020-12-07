7e8d68634992501d5c0fd5d44a26de67a23a4cdd
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-12-07 15:46:41 +0000

    Add search fn & captions file
    Opacity to hide not display none.
    right margin will break.
    Assume have to insert elements each time to achieve this.
    Increasing rendering each time.

---
+++ js/captions.js
@@ -0,0 +1,73 @@
+const captions = [
+  {
+    img: '01.jpg',
+    alt: 'Hay Bales',
+    description:
+      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
+  },
+  {
+    img: '02.jpg',
+    alt: 'Lake',
+    description:
+      'The lake was so calm today. We had a great view of the snow on the mountains from here.',
+  },
+  {
+    img: '03.jpg',
+    alt: 'Canyon',
+    description:
+      'I hiked to the top of the mountain and got this picture of the canyon and trees below.',
+  },
+  {
+    img: '04.jpg',
+    alt: 'Iceberg',
+    description:
+      'It was amazing to see an iceberg up close, it was so cold but didn’t snow today.',
+  },
+  {
+    img: '05.jpg',
+    alt: 'Desert',
+    description:
+      'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.',
+  },
+  {
+    img: '06.jpg',
+    alt: 'Fall',
+    description:
+      'Fall is coming, I love when the leaves on the trees start to change color.',
+  },
+  {
+    img: '07.jpg',
+    alt: 'Plantation',
+    description:
+      'I drove past this plantation yesterday, everything is so green!',
+  },
+  {
+    img: '08.jpg',
+    alt: 'Dunes',
+    description:
+      'My summer vacation to the Oregon Coast. I love the sandy dunes!',
+  },
+  {
+    img: '09.jpg',
+    alt: 'Countryside Lane',
+    description: 'We enjoyed a quiet stroll down this countryside lane.',
+  },
+  {
+    img: '10.jpg',
+    alt: 'Sunset',
+    description:
+      'Sunset at the coast! The sky turned a lovely shade of orange.',
+  },
+  {
+    img: '11.jpg',
+    alt: 'Cave',
+    description:
+      'I did a tour of a cave today and the view of the landscape below was breathtaking.',
+  },
+  {
+    img: '12.jpg',
+    alt: 'Bluebells',
+    description:
+      'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.',
+  },
+];
--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log('loaded!');
+const thumbnails = $('.photo');
+const searchbar = $('#search');
+
+const searchHandler = () => {
+  const text = searchbar.val().toLowerCase();
+  // take list of photos
+  thumbnails.each(function () {
+    const alt = $(this)
+      .children()
+      .first()
+      .children()
+      .first()
+      .attr('alt')
+      .toLowerCase();
+    if (alt.match(text)) {
+      $(this).attr('style', 'opacity: 1');
+    } else {
+      $(this).attr('style', 'opacity: 0');
+    }
+  });
+};
+
+searchbar.on('keyup', searchHandler);
