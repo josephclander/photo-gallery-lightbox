097a6cb504ae0d9bac8f77a1587fd5d6a2cc5e64
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-12-07 19:30:35 +0000

    Add modal functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const thumbnails = $('.photo');
 const searchbar = $('#search');
+let currentImage;
 
 const searchHandler = () => {
   const text = searchbar.val().toLowerCase();
@@ -22,18 +23,47 @@ const searchHandler = () => {
 
 searchbar.on('keyup', searchHandler);
 
+const imageLoader = (element) => {
+  $('#modal').css('display', 'block');
+
+  const path = $(element).children().first().attr('href');
+  const alt = $(element).children().first().children().first().attr('alt');
+  const caption = $(element).children().first().attr('data-title');
+  const imageInput = `<img src="${path}" alt="${alt}" />`;
+  $('#modal__image').html(imageInput);
+  $('#modal__caption').html(caption);
+};
+
 thumbnails.click(function (e) {
   e.preventDefault();
-  $('.modal').toggle();
-
-  const path = $(this).children().first().attr('href');
-  const alt = $(this).children().first().children().first().attr('alt');
-  const caption = $(this).children().first().attr('data-title');
-  const input = `<img src="${path}" alt="${alt}" />
-  <p>${caption}</p>`;
-  $('#modal__image').html(input);
+  imageLoader(this);
+  currentImage = $(this).index();
 });
 
 $('#close').click(function () {
   $('#modal').toggle();
+});
+
+$('#left_arrow').click(function () {
+  if (currentImage > 0) {
+    imageLoader(thumbnails[currentImage - 1]);
+    currentImage--;
+  }
+  if (currentImage === 0) {
+    $(this).css('color', 'rgb(30,30,30)');
+  } else {
+    $('.arrow').css('color', '#fff');
+  }
+});
+
+$('#right_arrow').click(function () {
+  if (currentImage < thumbnails.length - 1) {
+    imageLoader(thumbnails[currentImage + 1]);
+    currentImage++;
+  }
+  if (currentImage === thumbnails.length - 1) {
+    $(this).css('color', 'rgb(30,30,30)');
+  } else {
+    $('.arrow').css('color', '#fff');
+  }
 });
